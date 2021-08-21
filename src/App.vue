9e3d5b9f13a63d9a161cfcd1fdc9785f5cd7cc2e
k343979 <yusuke040989@gmail.com>
2021-08-22 02:22:40 +0900

    fixed some files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
-        <router-view/>
+        <Home />
     </div>
 </template>
+
+<script>
+    import Home from "./views/Home.vue";
+    export default {
+        data() {
+            return {
+                // 
+            }
+        },
+        components: {
+            Home
+        }
+    }
+</script>
 
 <style>
     #app {
@@ -17,7 +27,7 @@
         color: #2c3e50;
     }
 
-    #nav {
+    /* #nav {
         padding: 30px;
     }
 
@@ -28,5 +38,5 @@
 
     #nav a.router-link-exact-active {
         color: #42b983;
-    }
+    } */
 </style>
